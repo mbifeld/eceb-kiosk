0168166f955b5ccd3047566eab5ae2854a350fe9
Mitchell Bifeld <mbifeld@gmail.com>
2020-10-09 14:54:46 -0500

    Integrated Electron

--- eceb-kiosk/src/App.vue
+++ eceb-kiosk/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="nav">
+    <!--
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    !-->
   </div>
   <router-view/>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir, Helvetica, Arial, sans-serif";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -26,5 +28,10 @@
       color: #42b983;
     }
   }
+}
+
+@font-face {
+  font-family: "Proxmia Nova Bold";
+  src: url("assets/Proxima-Nova-Bold.otf");
 }
 </style>
--- eceb-kiosk/src/views/Home.vue
+++ eceb-kiosk/src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="touch-instruction"> TOUCH TO BEGIN </div> 
+    <div class="title"> The ECEB has saved over 500 Polar Bears to date! </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="touch-instruction bottom"> TOUCH TO BEGIN </div> 
   </div>
 </template>
 
@@ -16,3 +18,36 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.title {
+  color: blue;
+}
+
+.touch-instructions {
+  position: absolute;
+  left: 44.01%;
+  right: 43.96%;
+  top: 5.28%;
+  bottom: 92.87%;
+
+  font-family: "Proxima Nova Bold" !important;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 20px;
+
+  /* identical to box height */
+  text-align: right;
+  letter-spacing: 5px;
+
+  color: #FFFFFF;
+}
+
+.bottom {
+  top: 92.87% !important;
+  bottom: 5.28% !important;
+}
+
+</style>
+
