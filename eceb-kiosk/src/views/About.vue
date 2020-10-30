f360662320f11499751d83f28ce5a5e1895c6d5d
Mitchell Bifeld <mbifeld@gmail.com>
2020-10-30 03:15:22 -0500

    Set up initial project

---
+++ eceb-kiosk/src/views/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container fluid class="about page">
+    <v-container fluid id="about-title">
+      About Slide
+    </v-container>
+    <v-container fluid id="about-buttons">
+      <v-btn to="/" x-large id="back-button">Back</v-btn>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+
+  #about-title {
+    position: absolute;
+  }
+
+  #about-buttons {
+    position: absolute;
+    width: auto;
+    bottom: 5px;
+    right: 10px;
+  }
+
+  #back-button {
+    margin-right: 15px;
+  }
+
+</style>
---
+++ eceb-kiosk/src/views/Template.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid class="template page">
+    Template Here
+    <v-btn to="/" x-large id="back-button">Back</v-btn>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'Template'
+}
+
+</script>
+
+<style scoped>
+
+  #back-button {
+    position: absolute;
+    width: auto;
+    bottom: 20px;
+    right: 20px;
+  }
+
+</style>
